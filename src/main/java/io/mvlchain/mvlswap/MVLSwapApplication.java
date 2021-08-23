5243309c214f201b7f3dee9a6a328fd2d8f570db
DESKTOP-BHILVNI\foris <55five@gmail.com>
2021-08-23 16:52:59 +0900

    Initial Commit

---
+++ src/main/java/io/mvlchain/mvlswap/MVLSwapApplication.java
@@ -0,0 +1,13 @@
+package io.mvlchain.mvlswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MVLSwapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MVLSwapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mvlchain/mvlswap/MvlSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mvlchain.mvlswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MVLSwapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
