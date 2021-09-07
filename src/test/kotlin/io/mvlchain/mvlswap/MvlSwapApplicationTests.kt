ec873225bd45cb2d5b0949c6d86f172bdc0fc834
drunkhacker <drh@wafflestudio.com>
2021-09-07 21:57:56 +0900

    build: converted to gradle

---
+++ src/test/kotlin/io/mvlchain/mvlswap/MvlSwapApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.mvlchain.mvlswap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MvlSwapApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
